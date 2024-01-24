9456f9f3a45a9ea61da1b302f8a6efc945f36cc2
Bogdan Gromov <14brother@gmail.com>
2024-01-24 22:31:10 +0200

    fix: styles slider portfolio

--- src/app/features/portfolio/portfolio.component.ts
+++ src/app/features/portfolio/portfolio.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  DestroyRef,
+  HostListener,
+  ViewChild,
+  inject,
+} from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { debounceTime, fromEvent, takeUntil } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { CommonModule } from '@angular/common';
+
+const ITEM_WIDTH = 800;
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.css'
+  styleUrl: './portfolio.component.css',
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements AfterViewInit {
+  // #destroyRef = inject(DestroyRef);
+  @ViewChild('scrollContainer') scrollContainer: any;
 
+  disabledLeft: boolean = true;
+  disabledRight: boolean = false;
+
+  @HostListener('document:keydown', ['$event'])
+  setScroll(event: KeyboardEvent) {
+    if (event.key === 'ArrowLeft') {
+      console.log(event.key);
+      this.onScrollLeft();
+    }
+
+    if (event.key === 'ArrowRight') {
+      this.onScrollRight();
+    }
+  }
+
+  ngAfterViewInit() {}
+
+  onScrollLeft() {
+    // Offset / OffsetWidth
+    const container = this.scrollContainer.nativeElement as HTMLElement;
+    const scrollX = container.scrollLeft;
+
+    const scrollPosition = container.scrollWidth;
+
+    const result = scrollX - ITEM_WIDTH;
+    if (result <= scrollPosition) {
+      this.disabledLeft = true;
+    }
+    this.disabledRight = false;
+    container.scrollLeft = result;
+  }
+
+  onScrollRight() {
+    const container = this.scrollContainer.nativeElement as HTMLElement;
+
+    const scrollPosition = container.scrollWidth;
+    const scrollX = container.scrollLeft;
+
+    const result = scrollX + ITEM_WIDTH;
+    if (result >= scrollPosition) {
+      this.disabledRight = true;
+    }
+    this.disabledLeft = false;
+    container.scrollLeft = result;
+  }
 }
